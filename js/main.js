1a12e94b09c0d5bd28163037c40298448e712005
Luca <peruzzoluca98@gmail.com>
2021-04-28 16:50:22 +0200

    current page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-window.onresize = () => {
-    checkOnResize();
-}
-
-window.onload = () => {
-    checkOnResize();
-}
 const links = document.querySelectorAll("a");
 
 for (const link of links) {
@@ -20,22 +13,77 @@ function clickHandler(e) {
     }
 
     scroll({
-        top: offsetTop,
+        top: offsetTop - 120,
         behavior: "smooth"
     });
 }
 
-menu = document.querySelector('.burger-menu');
-nav = document.querySelector('nav');
 checkOnResize = () => {
-    if (window.outerWidth < 1025 && document.querySelector('.burger-menu').classList.contains('hidden')) {
-        this.menu.classList.remove('hidden');
-        this.nav.classList.add('hidden');
-    } else if (window.outerWidth >= 1025 && !document.querySelector('.burger-menu').classList.contains('hidden')) {
-        this.menu.classList.add('hidden');
-        this.nav.classList.remove('hidden');
+    if (window.outerWidth < 1025) {
+        document.querySelector('.burger-menu').classList.remove('hidden');
+        document.querySelector('nav').classList.add('hidden');
+    } else if (window.outerWidth >= 1025) {
+        document.querySelector('.burger-menu').classList.add('hidden');
+        document.querySelector('nav').classList.remove('hidden');
     }
 }
 toggleMenu = () => {
-    this.nav.classList.toggle('hidden');
+    document.querySelector('nav').classList.toggle('hidden');
+}
+currentLocationInPage = () => {
+    let currentPosition = window.scrollY;
+    const home = document.querySelector('#main');
+    const pages = document.querySelector('#pages');
+    const portfolio = document.querySelector('#portfolio');
+    const blog = document.querySelector('#blog');
+
+    const homeTop = 0
+    const homeBottom = home.getBoundingClientRect().bottom + window.scrollY;
+    const pagesTop = pages.getBoundingClientRect().top + window.scrollY;
+    const pagesBottom = pages.getBoundingClientRect().bottom + window.scrollY;
+    const portfolioTop = portfolio.getBoundingClientRect().top + window.scrollY;
+    const portfolioBottom = portfolio.getBoundingClientRect().bottom + window.scrollY;
+    const blogTop = blog.getBoundingClientRect().top + window.scrollY;
+    const blogBottom = blog.getBoundingClientRect().bottom + window.scrollY;
+
+    const homeLink = document.querySelector('#homeLink');
+    const pagesLink = document.querySelector('#pagesLink');
+    const portfolioLink = document.querySelector('#portfolioLink');
+    const blogLink = document.querySelector('#blogLink');
+
+    if (currentPosition >= homeTop) {
+        homeLink.classList.add('current-menu-item');
+        pagesLink.classList.remove('current-menu-item');
+        portfolioLink.classList.remove('current-menu-item');
+        blogLink.classList.remove('current-menu-item');
+    }
+    if (currentPosition >= pagesTop - 120) {
+        homeLink.classList.remove('current-menu-item');
+        pagesLink.classList.add('current-menu-item');
+        portfolioLink.classList.remove('current-menu-item');
+        blogLink.classList.remove('current-menu-item');
+    }
+    if (currentPosition >= portfolioTop - 120) {
+        homeLink.classList.remove('current-menu-item');
+        pagesLink.classList.remove('current-menu-item');
+        portfolioLink.classList.add('current-menu-item');
+        blogLink.classList.remove('current-menu-item');
+    }
+    if (currentPosition >= blogTop - 120) {
+        homeLink.classList.remove('current-menu-item');
+        pagesLink.classList.remove('current-menu-item');
+        portfolioLink.classList.remove('current-menu-item');
+        blogLink.classList.add('current-menu-item');
+    }
+
+}
+window.onresize = () => {
+    checkOnResize();
+}
+
+window.onload = () => {
+    checkOnResize();
+}
+window.onscroll = () => {
+    currentLocationInPage();
 }
